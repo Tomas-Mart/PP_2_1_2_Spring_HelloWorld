3b48a6f83795bdc60088e5cf28430ead471b5834
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-03-09 14:50:40 +0400

    Spring. Первые шаги. HelloWorld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println("Сравнение бинов helloworld: " + (bean1 == bean2));
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getMessage());
+
+        System.out.println("Сравнение бинов cat: " + (bean3 == bean4));
     }
 }
